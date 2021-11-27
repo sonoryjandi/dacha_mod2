5e088bb86f4ff8f0cc4df049329b186afd2cf12b
Nana <realyeolly@gmail.com>
2021-11-27 16:09:37 +0500

    Пробник ArrayList.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("тест пушкамит");
+        ArrayList testArray = new ArrayList();
+        testArray.add("rex");
+        testArray.add("yoos");
+        testArray.add("nap");
+        testArray.add("kris");
+//        test.print();
+        testArray.remove(0);
     }
 }
