922970d83a7e5ab6fd40b58456cb8d99f5680fee
Nana <realyeolly@gmail.com>
2021-11-30 23:28:32 +0500

    ArrayList complete

--- src/main/java/ArrayList.java
+++ src/main/java/ArrayList.java
@@ -1,71 +1,344 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import com.sun.java.swing.plaf.windows.WindowsTextAreaUI;
 
-public class ArrayList implements List {
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.copyOf;
+import static java.util.Arrays.copyOfRange;
+
+public class ArrayList<R> implements List<R> {
 
     private Object[] array;
+    private int size;
+
+    public ArrayList(Object[] array) {
+        this.array = copyOf(array, array.length);
+        this.size = array.length;
+    }
 
     public ArrayList() {
         this.array = new Object[0];
+        this.size = 0;
     }
 
-    @Override
-    public boolean add(Object object) {
-        array = Arrays.copyOf(array, array.length + 1);
-        array[array.length - 1] = object;
-        return true;
+    public ArrayList(int length) {
+        this.array = new Object[length];
+        this.size = 0;
     }
 
-    @Override
-    public void remove(int index) {
-        array[index] = null;
-        for (int counter = index; counter < array.length; counter++){
-            array[counter] = array[index + 1];
-            array[index + 1] = null;
-        }
-        array = Arrays.copyOf(array, array.length - 1);
-//        Object[] newArray = new Object[array.length - 1];
-//        for (int i = 0; i < array.length; i++) {
-//            array[index] = null;
-//            if (array[i] = array[index]){
-//                continue;
-//            }
-//            newArray[]
-//            array[i] = array[i + 1];
-//            array[i + 1] = null;
-//        }
-//        array = Arrays.copyOf(array, array.length - 1);
-    }
-
+    /**
+     * Returns the number of elements in this list.  If this list contains
+     * more than <tt>Integer.MAX_VALUE</tt> elements, returns
+     * <tt>Integer.MAX_VALUE</tt>.
+     *
+     * @return the number of elements in this list
+     */
     @Override
     public int size() {
-        return array.length;
+        return size;
     }
 
+    /**
+     * Returns <tt>true</tt> if this list contains no elements.
+     *
+     * @return <tt>true</tt> if this list contains no elements
+     */
     @Override
     public boolean isEmpty() {
-        return array.length == 0;
+        return size == 0;
     }
 
+    /**
+     * Returns <tt>true</tt> if this list contains the specified element.
+     * More formally, returns <tt>true</tt> if and only if this list contains
+     * at least one element <tt>e</tt> such that
+     * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
+     *
+     * @param o element whose presence in this list is to be tested
+     * @return <tt>true</tt> if this list contains the specified element
+     * @throws ClassCastException   if the type of the specified element
+     *                              is incompatible with this list
+     *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException if the specified element is null and this
+     *                              list does not permit null elements
+     *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+     */
     @Override
-    public boolean contains(Object name) {
-        for (Object object : array) {
-            if (object.equals(name)) {
+    public boolean contains(Object o) {
+        for (Object element : array){
+            if (element.equals(o)){
                 return true;
             }
         }
         return false;
     }
 
+    /**
+     * Appends the specified element to the end of this list (optional
+     * operation).
+     *
+     * <p>Lists that support this operation may place limitations on what
+     * elements may be added to this list.  In particular, some
+     * lists will refuse to add null elements, and others will impose
+     * restrictions on the type of elements that may be added.  List
+     * classes should clearly specify in their documentation any restrictions
+     * on what elements may be added.
+     *
+     * @param r element to be appended to this list
+     * @return <tt>true</tt> (as specified by {@link Collection#add})
+     * @throws UnsupportedOperationException if the <tt>add</tt> operation
+     *                                       is not supported by this list
+     * @throws ClassCastException            if the class of the specified element
+     *                                       prevents it from being added to this list
+     * @throws NullPointerException          if the specified element is null and this
+     *                                       list does not permit null elements
+     * @throws IllegalArgumentException      if some property of this element
+     *                                       prevents it from being added to this list
+     */
     @Override
-    public Object[] subList(int fromIndex, int toIndex) {
+    public boolean add(R r) {
+        array = copyOf(array, array.length + 1);
+        array[size] = r;
+        size++;
+        return true;
+    }
 
+    /**
+     * Removes the first occurrence of the specified element from this list,
+     * if it is present (optional operation).  If this list does not contain
+     * the element, it is unchanged.  More formally, removes the element with
+     * the lowest index <tt>i</tt> such that
+     * <tt>(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i)))</tt>
+     * (if such an element exists).  Returns <tt>true</tt> if this list
+     * contained the specified element (or equivalently, if this list changed
+     * as a result of the call).
+     *
+     * @param o element to be removed from this list, if present
+     * @return <tt>true</tt> if this list contained the specified element
+     * @throws ClassCastException            if the type of the specified element
+     *                                       is incompatible with this list
+     *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException          if the specified element is null and this
+     *                                       list does not permit null elements
+     *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @throws UnsupportedOperationException if the <tt>remove</tt> operation
+     *                                       is not supported by this list
+     */
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < size; i++){
+            if (array[i].equals(o)){
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Inserts the specified element at the specified position in this list
+     * (optional operation).  Shifts the element currently at that position
+     * (if any) and any subsequent elements to the right (adds one to their
+     * indices).
+     *
+     * @param index   index at which the specified element is to be inserted
+     * @param element element to be inserted
+     * @throws UnsupportedOperationException if the <tt>add</tt> operation
+     *                                       is not supported by this list
+     * @throws ClassCastException            if the class of the specified element
+     *                                       prevents it from being added to this list
+     * @throws NullPointerException          if the specified element is null and
+     *                                       this list does not permit null elements
+     * @throws IllegalArgumentException      if some property of the specified
+     *                                       element prevents it from being added to this list
+     * @throws IndexOutOfBoundsException     if the index is out of range
+     *                                       (<tt>index &lt; 0 || index &gt; size()</tt>)
+     */
+    @Override
+    public void add(int index, R element) {
+        array = copyOf(array, array.length + 1);
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = element;
+        size++;
+    }
+
+    /**
+     * Removes the element at the specified position in this list (optional
+     * operation).  Shifts any subsequent elements to the left (subtracts one
+     * from their indices).  Returns the element that was removed from the
+     * list.
+     *
+     * @param index the index of the element to be removed
+     * @return the element previously at the specified position
+     * @throws UnsupportedOperationException if the <tt>remove</tt> operation
+     *                                       is not supported by this list
+     * @throws IndexOutOfBoundsException     if the index is out of range
+     *                                       (<tt>index &lt; 0 || index &gt;= size()</tt>)
+     */
+    @Override
+    public R remove(int index) {
+        R element = (R) array[index];
+        for (int i = index; i < size - 1; i++){
+            array[i] = array[i + 1];
+        }
+        size--;
+        return element;
+    }
+
+    /**
+     * Returns a view of the portion of this list between the specified
+     * <tt>fromIndex</tt>, inclusive, and <tt>toIndex</tt>, exclusive.  (If
+     * <tt>fromIndex</tt> and <tt>toIndex</tt> are equal, the returned list is
+     * empty.)  The returned list is backed by this list, so non-structural
+     * changes in the returned list are reflected in this list, and vice-versa.
+     * The returned list supports all of the optional list operations supported
+     * by this list.<p>
+     * <p>
+     * This method eliminates the need for explicit range operations (of
+     * the sort that commonly exist for arrays).  Any operation that expects
+     * a list can be used as a range operation by passing a subList view
+     * instead of a whole list.  For example, the following idiom
+     * removes a range of elements from a list:
+     * <pre>{@code
+     *      list.subList(from, to).clear();
+     * }</pre>
+     * Similar idioms may be constructed for <tt>indexOf</tt> and
+     * <tt>lastIndexOf</tt>, and all of the algorithms in the
+     * <tt>Collections</tt> class can be applied to a subList.<p>
+     * <p>
+     * The semantics of the list returned by this method become undefined if
+     * the backing list (i.e., this list) is <i>structurally modified</i> in
+     * any way other than via the returned list.  (Structural modifications are
+     * those that change the size of this list, or otherwise perturb it in such
+     * a fashion that iterations in progress may yield incorrect results.)
+     *
+     * @param fromIndex low endpoint (inclusive) of the subList
+     * @param toIndex   high endpoint (exclusive) of the subList
+     * @return a view of the specified range within this list
+     * @throws IndexOutOfBoundsException for an illegal endpoint index value
+     *                                   (<tt>fromIndex &lt; 0 || toIndex &gt; size ||
+     *                                   fromIndex &gt; toIndex</tt>)
+     */
+    @Override
+    public List<R> subList(int fromIndex, int toIndex) {
+        Object[] subArray = copyOfRange(array, fromIndex, toIndex);
+        return new ArrayList<R>(subArray);
+    }
+
+    // region methods
+    @Override
+    public Iterator<R> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
         return new Object[0];
     }
 
-    public void print() {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
     }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends R> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends R> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public R get(int index) {
+        return null;
+    }
+
+    @Override
+    public R set(int index, R element) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<R> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<R> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<R> operator) {
+
+    }
+
+    @Override
+    public void sort(Comparator<? super R> c) {
+
+    }
+
+    @Override
+    public Spliterator<R> spliterator() {
+        return null;
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super R> filter) {
+        return false;
+    }
+
+    @Override
+    public Stream<R> stream() {
+        return null;
+    }
+
+    @Override
+    public Stream<R> parallelStream() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super R> action) {
+
+    }
+    // endregion
 }
